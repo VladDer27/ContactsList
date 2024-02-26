6b47102cfd931845b452b686e10ca1a3d793d682
vladislav_derzhavin <cadoni827@gmail.com>
2024-02-26 14:03:49 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/ContactsListApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactsListApplication.class, args);
+	}
+
+}
